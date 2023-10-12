48d9f8d090a12bdb6ce4897436e6091249f89c7f
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-10-12 18:24:21 +0200

    primer commit

---
+++ app/src/main/java/com/example/promul_bundle_logcat_ciclovidadeapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.promul_bundle_logcat_ciclovidadeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
